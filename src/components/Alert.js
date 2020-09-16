9e9119832b9a2fcd6b01c7b670170fa5889eff44
Vladimir Pravotorov <jtone028@gmail.com>
2020-09-16 23:08:35 +0300

    add animations

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,19 +1,25 @@
 import React, {useContext} from 'react';
 import alertContext from "../context/alert/alertContext";
+import {CSSTransition} from 'react-transition-group'
 
 export const Alert = () => {
     const {alert, hide} = useContext(alertContext)
 
-    if(!alert.visible) return null
-
-
     return (
-        <div className={`alert alert-${alert.type || 'warning'} alert-dismissible`} >
-            <strong>Внимание!</strong>
-            &nbsp; {alert.text}
-            <button onClick={hide} type="button" className="close" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
-        </div>
+        <CSSTransition
+        in={alert.visible}
+        timeout={750}
+        className={'alert'}
+        mountOnEnter
+        unmountOnExit
+        >
+            <div className={`alert alert-${alert.type || 'warning'} alert-dismissible`} >
+                <strong>Внимание!</strong>
+                &nbsp; {alert.text}
+                <button onClick={hide} type="button" className="close" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+        </CSSTransition>
     )
 }
--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,24 +1,31 @@
 import React from 'react'
+import {TransitionGroup, CSSTransition} from 'react-transition-group'
 
 export const Notes = ( { notes, onRemove }) => (
-    <ul className="list-group">
-        { notes.map(note => (<li
-            className="list-group-item note"
+    < TransitionGroup component="ul" className="list-group">
+        { notes.map(note => (
+        <CSSTransition
             key={note.id}
+            className="note"
+            timeout={800}
         >
-        <div>
-            <strong>{note.title}</strong>
-            {/*TODO edit date */}
-            <small>{new Date().toLocaleDateString()}</small>
-        </div>
-        {/*TODO add alert to delete*/}
-        <button
-            type="button"
-            className="btn btn-outline-danger btn-sm"
-            onClick={() => onRemove(note.id)}
-            > &times;
-        </button>
-        </li>
+            <li
+                className="list-group-item note"
+            >
+            <div>
+                <strong>{note.title}</strong>
+                {/*TODO edit date */}
+                <small>{new Date().toLocaleDateString()}</small>
+            </div>
+            {/*TODO add alert to delete*/}
+            <button
+                type="button"
+                className="btn btn-outline-danger btn-sm"
+                onClick={() => onRemove(note.id)}
+                > &times;
+            </button>
+            </li>
+        </CSSTransition>
         ))}
-    </ul>
+    </TransitionGroup>
 )
